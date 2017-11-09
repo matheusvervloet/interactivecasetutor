9636b1d1bdce0760c93a250cccee9450e00e6356
Matheus <matheus@debian-512mb-sfo2-01.localdomain>
2017-11-09 15:42:18 +0000

    added web server

---
+++ server/chat_server.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify
+import socket, random
+
+app = Flask(__name__, static_url_path='/static')
+
+cs_host = "localhost"
+cs_port = 1024
+cs_addr = (cs_host,cs_port)
+
+cs_bot = 'anabotelho'
+cs_master = 'm4th3u5'
+
+@app.route('/')
+def index():
+		resp = app.make_response(redirect_to_index )  
+
+@app.route('/static/response', methods=['POST'])
+def response():
+		result = request.get_json()
+		username = request.remote_addr + request.headers.get('User-Agent')
+		message = username+chr(0)+cs_bot+chr(0)+result+chr(0)
+		if len(result):
+			split_result = result.split(' ')[0]
+			if split_result==":reset" or split_result==":pos" or split_result==":why" or split_result==":build":
+				message = cs_master+chr(0)+cs_bot+chr(0)+result+chr(0)
+		message = message.encode("utf-8")
+		client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		client_socket.bind(("localhost",random.randrange(1025)+1024))
+		client_socket.connect(cs_addr)
+		client_socket.send(message)
+		data_out = ''
+		while True:
+			buf = client_socket.recv(64)
+			if len(buf) > 0:
+				data_out += buf.decode("utf-8")
+			else:
+				break
+		client_socket.close()
+		return jsonify(data_out)
+
+
+if __name__ == "__main__":
+	app.run(host="165.227.49.154", threaded=True)
+
+
+
+'''if result[len(result) - len(result.lstrip())] == ':':
+				if result.rsplit(':',3)[1] == 'pWd':
+					result = ':'+result.rsplit(':',3)[2]
+				else:
+					result = result.rsplit(':',3)[1]'''
---
+++ server/static/app.js
@@ -0,0 +1,76 @@
+(function () {
+    var Message;
+    Message = function (arg) {
+        this.text = arg.text, this.message_side = arg.message_side;
+        this.draw = function (_this) {
+            return function () {
+                var $message;
+                if(_this.message_side=='right'){
+                    $message = $($('.message_template_right').clone().html());
+                }
+                else{
+                    $message = $($('.message_template_left').clone().html());
+                }
+                $message.addClass(_this.message_side).find('.text').html(_this.text);
+                $('.messages').append($message);
+                return setTimeout(function () {
+                    return $message.addClass('appeared');
+                }, 0);
+            };
+        }(this);
+        return this;
+    };
+    $(function () {
+        var getMessageText, printMessage;
+        getMessageText = function () {
+            var $message_input;
+            $message_input = $('.message_input');
+            return $message_input.val();
+        };
+        printMessage = function (text, message_side) {
+            var $messages, message;
+            if (text.trim() === '') {
+                return;
+            }
+            $('.message_input').val('');
+            $messages = $('.messages');
+            message = new Message({
+                text: text,
+                message_side: message_side
+            });
+            message.draw();
+            return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+        };
+        sendMessage = function (text) {
+            response_text = ''
+            $.ajax({
+                    url:"response",
+                    type:"POST",
+                    data:JSON.stringify(text),
+                    contentType:"application/json; charset=utf-8",
+                    dataType:"json",
+                    success: function(data){
+                                timer = data.split('\\n')[0].split(' ').length * 100;
+                                data.split('\\n').forEach(function (item, index) {
+                                    setTimeout(function (){return printMessage(item, 'left')}, timer);
+                                    timer += (item.split(' ').length)*100;
+                                });
+                            }
+            });
+            return response_text;
+        };
+        $('.send_message').click(function (e) {
+            input_text = getMessageText();
+            printMessage(input_text, 'right');  //print input
+            sendMessage(input_text); //print response
+        });
+        $('.message_input').keyup(function (e) {
+            if (e.which === 13) {
+                input_text = getMessageText();
+                printMessage(input_text, 'right');  //print input
+                sendMessage(input_text); //print response
+            }
+        });
+        sendMessage('');
+    });
+}.call(this));
